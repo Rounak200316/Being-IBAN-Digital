57837b4d58ee5a7d3914a148ea362bdf7361eaa4
Rounak Dey Sarkar <rounakdeysarkar@gmail.com>
2026-08-30 22:23:57 +0530

    changes in backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,16 @@
 import express from "express";
-
 import cors from "cors";
-
 import "dotenv/config";
 
 import connectDb from "./config/monodb.js";
 
 import userRouter from "./routes/userRoutes.js";
-
 import productRouter from "./routes/productRoutes.js";
-
 import cartRouter from "./routes/cartRoutes.js";
-
 import orderRouter from "./routes/orderRoutes.js";
 import meetingRouter from "./routes/meetingRoutes.js";
-import sendEmail from "./utils/email.js";
-
 
 const app = express();
-
-const port = process.env.PORT || 4000;
-
-
-// =====================================================
-// CONNECT DATABASE
-// =====================================================
-
-connectDb();
-
 
 // =====================================================
 // MIDDLEWARE
@@ -37,58 +20,35 @@ app.use(express.json());
 
 app.use(cors());
 
+// =====================================================
+// DATABASE
+// =====================================================
+
+connectDb();
 
 // =====================================================
 // ROUTES
 // =====================================================
 
-app.use(
-    "/api/user",
-    userRouter
-);
-
-app.use(
-    "/api/product",
-    productRouter
-);
-
-app.use(
-    "/api/cart",
-    cartRouter
-);
-
-app.use(
-    "/api/order",
-    orderRouter
-);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 app.use("/api/meeting", meetingRouter);
 
 // =====================================================
 // TEST API
 // =====================================================
 
-app.get(
-    "/",
-    (req, res) => {
-
-        res.send("API working");
-
-    }
-);
-
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Being IBAN Digital API working",
+    });
+});
 
 // =====================================================
-// START SERVER
+// VERCEL
 // =====================================================
 
-app.listen(
-    port,
-    () => {
-
-        console.log(
-            "Server started on port " + port
-        );
-
-    }
-);
-
+export default app;
